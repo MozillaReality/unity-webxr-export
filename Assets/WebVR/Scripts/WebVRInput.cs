c7d96a835145009ce3ab7ea06aac86cdeeb5830c
caseyyee <caseyyee.ca@gmail.com>
2018-02-01 21:42:51 -0800

    Adds Input and interaction script

---
+++ Assets/WebVR/Scripts/WebVRInput.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebVRInput : MonoBehaviour {
+	private Interaction interaction = null;
+	private int Joysticks = 0;
+
+	void Awake() {
+		interaction = GetComponent<Interaction>();
+	}
+
+	void Update() {
+		if (Input.GetKey("joystick button 1")) {
+			interaction.Pickup ();
+		} 
+
+		if (Input.GetKeyUp("joystick button 1")) {
+			interaction.Drop ();
+		}
+	}
+}
