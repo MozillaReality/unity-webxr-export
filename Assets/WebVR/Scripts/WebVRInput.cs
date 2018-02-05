268ca3b7327f30eae9828c6a1c50c39f67f15da1
caseyyee <caseyyee.ca@gmail.com>
2018-02-05 12:32:21 -0800

    differentiate interaction between controllers

--- Assets/WebVR/Scripts/WebVRInput.cs
+++ Assets/WebVR/Scripts/WebVRInput.cs
@@ -3,20 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WebVRInput : MonoBehaviour {
-	private Interaction interaction = null;
-	private int Joysticks = 0;
+//	private Interaction interaction = null;
+	private WebVRCamera webVRCamera = null;
 
 	void Awake() {
-		interaction = GetComponent<Interaction>();
+//		interaction = GetComponent<Interaction>();
+		webVRCamera = GetComponentInParent<WebVRCamera> ();
 	}
 
 	void Update() {
-		if (Input.GetKey("joystick button 1")) {
-			interaction.Pickup ();
-		} 
 
-		if (Input.GetKeyUp("joystick button 1")) {
-			interaction.Drop ();
+//		public bool GetPress(ulong buttonMask) { Update(); return (state.ulButtonPressed & buttonMask) != 0; }
+//		public bool GetPressDown(ulong buttonMask) { Update(); return (state.ulButtonPressed & buttonMask) != 0 && (prevState.ulButtonPressed & buttonMask) == 0; }
+//		public bool GetPressUp(ulong buttonMask) { Update(); return (state.ulButtonPressed & buttonMask) == 0 && (prevState.ulButtonPressed & buttonMask) != 0; }
+
+
+//		string[] names = Input.GetJoystickNames();
+
+		// log joystick buttons as they are pressed.
+//		for(int i = 0; i < names.Length; i++) {
+//			for (int j = 0; j < 10; j++) {
+//				string joy = "joystick " + (i + 1) + " button " + j;
+//				if (Input.GetKeyDown (joy)) {
+//					Debug.Log(names[i] + " = " + joy);
+//				}
+//			}
+//		}
+
+//		if (webVRCamera.GetKeyDown(gameObject, 1)) {
+//			Debug.Log(gameObject.name + " button 1 down!!!!");
+//		}
+//
+//		if (webVRCamera.GetKeyUp(gameObject, 1)) {
+//			Debug.Log(gameObject.name + " button 1 up!!!!");
+//		}
+
+		if (webVRCamera.GetKey(gameObject, 1)) {
+			Debug.Log(gameObject.name + " button 1!!!!");
 		}
+
+//		if (webVRCamera.GetDown(gameObject, 1)) {
+//			Debug.Log(gameObject.name + " button 1 down !!!!");
+//		}
+
+//		if (controllerId != null) {
+//			if (Input.GetKeyDown ("joystick " + controllerId + " button 1")) {
+//				Debug.Log ("joy " + controllerId + " down");
+//				//interaction.Pickup ();
+//			} 
+//
+//			if (Input.GetKeyUp ("joystick " + controllerId + " button 1")) {
+//				Debug.Log ("joy " + controllerId + " up");
+//				//interaction.Drop ();
+//			}
+//		}
 	}
 }
