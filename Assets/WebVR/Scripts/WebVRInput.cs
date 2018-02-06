9014b106c40ec5402baa5b5acc2c128c10a07c76
caseyyee <caseyyee.ca@gmail.com>
2018-02-05 21:02:51 -0800

    working pickup and drop

--- Assets/WebVR/Scripts/WebVRInput.cs
+++ Assets/WebVR/Scripts/WebVRInput.cs
@@ -12,30 +12,12 @@ public class WebVRInput : MonoBehaviour {
 	}
 
 	void Update() {
-		if (webVRCamera.GetKey(gameObject, 1)) {
-			Debug.Log(gameObject.name + " button 1 getkey !!!!");
-		}
-
 		if (webVRCamera.GetKeyDown(gameObject, 1)) {
-			Debug.Log(gameObject.name + " button 1 getkeydown !!!!");
 			interaction.Pickup ();
 		}
 
 		if (webVRCamera.GetKeyUp(gameObject, 1)) {
-			Debug.Log(gameObject.name + " button 1 getkeup !!!!");
 			interaction.Drop ();
 		}
-
-//		if (controllerId != null) {
-//			if (Input.GetKeyDown ("joystick " + controllerId + " button 1")) {
-//				Debug.Log ("joy " + controllerId + " down");
-//				//interaction.Pickup ();
-//			} 
-//
-//			if (Input.GetKeyUp ("joystick " + controllerId + " button 1")) {
-//				Debug.Log ("joy " + controllerId + " up");
-//				//interaction.Drop ();
-//			}
-//		}
 	}
 }
