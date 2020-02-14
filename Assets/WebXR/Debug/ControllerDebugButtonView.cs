66d0f830f3a39e3cf20fb33b489da40eacca8d0e
Brian Turner <brian@brianturner.info>
2020-02-14 16:56:38 -0500

    Moved files around to better export into package

---
+++ Assets/WebXR/Debug/ControllerDebugButtonView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControllerDebugButtonView : MonoBehaviour
+{
+    [SerializeField]
+    private Text index;
+    [SerializeField]
+    private Toggle pressed;
+    [SerializeField]
+    private Text value;
+
+    public int Index
+    {
+        set { index.text = value.ToString(); }
+    }
+
+    public bool Pressed
+    {
+        set { pressed.isOn = value; }
+    }
+
+    public float Value
+    {
+        set { this.value.text = value.ToString(CultureInfo.InvariantCulture); }
+    }
+}
