98db049aea550120fc11e438858f3eb309b63141
Brian Turner <brian@brianturner.info>
2020-03-03 22:18:17 -0500

    This line is causing errors. Needs investigating

--- Assets/WebGLTemplates/WebXR/webxr.js
+++ Assets/WebGLTemplates/WebXR/webxr.js
@@ -155,7 +155,6 @@
   });
 
   XRManager.prototype.unityLoaded = async function () {
-    MozillaResearch.telemetry.performance.measure('LoadingTime', 'LoadingStart');
     document.body.dataset.unityLoaded = 'true';
 
     this.setGameInstance(await this.unityProgressStart);
